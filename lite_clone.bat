3e21a50c68676b1911b74d827d9fb9adc13752c5
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-09-25 18:01:58 +0100

    Update lite_clone.bat

--- lite_clone.bat
+++ lite_clone.bat
@@ -1,3 +1,4 @@
 REM This produces an 80MB folder to work on, instead of a 500MB one. 
+REM At least until you push to the remote with github desktop, then it goes back up to 200 MB with the entire git history 
 
 git clone --depth=1 --branch lite https://github.com/JamesParrott/sdna_open
